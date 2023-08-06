9e7fa382207bd304975b956ec6a5d90d44d92636
jiikko <n905i.1214@gmail.com>
2023-08-06 18:40:21 +0900

    ファイルの存在チェックを実装する

--- internal/commands/install_version.go
+++ internal/commands/install_version.go
@@ -3,6 +3,7 @@ package commands
 import (
 	"fmt"
 	"main/internal"
+	"os"
 )
 
 func downloadVersion(client HTTPClient, version string) error {
@@ -13,22 +14,34 @@ func prepareDirectory(installBaseDir string) error {
 	return nil
 }
 
-func validateDiretory(installBaseDir string) error {
-	return nil
+func existsVersionDirectory(installBaseDir string, version string) (bool, error) {
+	pbmVersionPath := installBaseDir + "/v" + version
+	if _, err := os.Stat(pbmVersionPath); err == nil {
+		// fmt.Printf("ファイル %s は存在します。\n", pbmVersionPath)
+		return true, nil
+	} else if os.IsNotExist(err) {
+		// fmt.Printf("ファイル %s は存在しません。\n", pbmVersionPath)
+		return false, nil
+	} else {
+		return false, err
+	}
 }
 
 func InstallVersion(client HTTPClient, version string, useOption bool, installBaseDir string) error {
-	fmt.Println(version)
-	fmt.Println(useOption)
-	fmt.Println(installBaseDir)
-
 	exists, err := internal.PBMGithubClient(client).ExistsVersion(version)
 	if err != nil {
 		return err
 	}
-
 	if !exists {
 		return fmt.Errorf("Version %s does not exist", version)
+	}
+
+	exists, err = existsVersionDirectory(installBaseDir, version)
+	if err != nil {
+		return err
+	}
+	if exists {
+		return fmt.Errorf("Version %s already installed", version)
 	}
 
 	downloadVersion(client, version)
